69a56e1a418093f4cbfaeb03e3cb04a6550fcec6
Sophie <totalsophie@gmail.com>
2022-01-13 15:33:40 +0300

    Created a rock paper scissors game where user input is prompted on the browser

---
+++ script.js
@@ -0,0 +1,90 @@
+// Creating a function computerPlay that randomly selects rock paper or scissors
+function computerPlay(){
+    let computer_choices = ['rock', 'paper', 'scissors'];
+    let random_number = Math.floor(Math.random() * 3);
+    
+    return (computer_choices[random_number]);
+}
+
+// function that takes parameters playerSelection, computerSelection and chooses who is the winner
+function playRound(playerSelection, computerSelection){
+    playerSelection = playerSelection.toLowerCase();
+
+    //If you both choose the same thing, it is a tie
+    if (computerSelection === 'rock' && playerSelection === 'rock'){
+        return("It's a tie")
+    }
+    
+    if (computerSelection === 'paper' && playerSelection === 'paper'){
+        return("It's a tie")
+    }
+
+    if (computerSelection === 'scissors' && playerSelection === 'scissors'){
+        return("It's a tie")
+    }
+
+    // Selection where you beat the computer
+    if (computerSelection === 'rock' && playerSelection === 'paper'){
+        return("You win")
+    }
+    if (computerSelection === 'paper' && playerSelection === 'scissors'){
+        return("You win")
+    }
+    if (computerSelection === 'scissors' && playerSelection === 'rock'){
+        return("You win")
+    }
+
+    //Selection where the computer beat you
+    else{
+        return ('You lose')
+    }
+
+}
+
+// Creating a function that keeps score and reports a winner or loser at the end. 
+function game(num){
+    let playerCounter = 0;
+    let computerCounter = 0;
+
+    for (let i = 0; i < num; i++){
+        let playerSelection = compare_input();
+        const computerSelection = computerPlay();
+        console.log(playRound(playerSelection, computerSelection));
+        result = playRound(playerSelection, computerSelection);
+
+        if (result == 'You lose'){
+            computerCounter = computerCounter + 1;
+        }
+        else if (result == 'You win'){
+            playerCounter = playerCounter + 1;     
+        }
+    }
+
+    if (computerCounter > playerCounter){
+        console.log('Sorry, You lose');
+    }
+    else if (playerCounter > computerCounter){
+        console.log('Yaaayy, You win');
+    }
+    else{
+        console.log('Well, you tied!')
+    }
+}
+
+    // Function that checks to see if the input is either rock, papaer or scissors
+function compare_input (){
+    let player_input = prompt('What is your weapon?');
+    player_input = player_input.toLowerCase();
+    let weapons = ['rock', 'paper', 'scissors'];
+
+
+    //if not.. reprompt till you get right answer
+    while (weapons.indexOf(player_input) < 0 ) //If the user number matches one of preset numbers
+    {
+        let player_input = prompt('What is your weapon?');
+    } 
+    return player_input;
+}
+
+//play how many times?
+game(3);
